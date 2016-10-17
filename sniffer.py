ff6ba0dce5f89134b2d89ab4c73cd4b82b7e027e
Mantarus <m.mantarus@gmail.com>
2016-10-18 01:26:31 +0300

    Add 'Socket could not be created' exception

--- sniffer.py
+++ sniffer.py
@@ -2,9 +2,14 @@
 # For Linux
 
 import socket
+import sys
 
 # create an INET, raw socket
-conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+try:
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+except socket.error as msg:
+    print('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
 
 # receive a packet
 while True:
